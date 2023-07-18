cba23b84ca6ac6c9bb4518c748b8b2e637d1f8d6
oscar-cortez-home <oscarkortez@gmail.com>
2023-07-18 16:52:12 -0400

    unitest poc

--- python/test-examples/code1.py
+++ python/test-examples/code1.py
@@ -1,6 +1,9 @@
 def total(my_list):
     """ returns the sum of the elements of a list """
 
+    if type(my_list) == int :
+        return (my_list)
+
     result : float = 0.0
 
     for item in my_list:
--- python/test-examples/code1_test.py
+++ python/test-examples/code1_test.py
@@ -3,8 +3,10 @@ from code1 import total
 
 def test_total():
     #The use cases :
+    assert(total([12.0, 2.0, 2.0])) == 16.0
+
     """The sum of several elements of a list must be correct"""
-    assert(total([1.0, 2.0, 3.0])) == 6.0
+    assert(total([1.1, 2.0, 3.0])) == 6.1
 
     """1 - 1 = 0"""
     assert total([1,-1]) == 0
@@ -18,3 +20,8 @@ def test_total():
 
     """The sum of an empty list must be 0"""
     assert total([]) == 0
+
+    assert total(10) == 10
+
+def test_total2():
+    assert total(10) == 10
